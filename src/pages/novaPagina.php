618c9a1cc54fc1de0675e8607d070b265815082b
Davi Ferreira <davi.caridade13@gmail.com>
2025-03-31 21:04:22 -0300

    criação de pagina tal

---
+++ src/pages/novaPagina.php
@@ -0,0 +1,13 @@
+<section class="p-4 flex-1 ml-0 sm:ml-64 sm:p-8">
+    <button id="menu-toggle" class="md:hidden p-2 fixed top-2 right-3 text-slate-700">
+        <span class="iconify text-4xl" data-icon="solar:hamburger-menu-line-duotone"></span>
+    </button>
+    <h2 class="text-2xl font-semibold">Nova pagina</h2>
+
+    <section class="mt-8 bg-white p-6 rounded shadow flex flex-col items-center gap-2 sm:flex-row">
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor autem recusandae doloribus ducimus sequi deserunt laborum suscipit nemo, dolorem nisi aperiam doloremque quis fuga voluptas quos accusantium tempore atque perferendis?</p>
+        </div>
+    </section>
+    <script src="src/js/main.js"></script>
+</section>
