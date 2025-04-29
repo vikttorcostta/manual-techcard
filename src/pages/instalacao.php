fcec528f3bbf694cbf473de80974e1df7162ab34
Davi Ferreira <davi.caridade13@gmail.com>
2025-04-28 21:10:18 -0300

    :pencil: Implemenntação de linhas de textos na seção de Instalação e Acesso

--- src/pages/instalacao.php
+++ src/pages/instalacao.php
@@ -6,7 +6,71 @@
 
     <section class="mt-8 bg-white p-6 rounded shadow flex flex-col items-center gap-2 sm:flex-row">
         <div>
-            <p>Edinaldo Pereira?</p>
+            <p>
+                Para instalar o sistema corretamente, siga os passos abaixo:<br><br>
+
+                <h2 class="text-2xl font-semibold flex items-center gab-3">
+                <span class="iconify text-2xl" data-icon="solar:round-alt-arrow-right-line-duotone"></span>
+                Requisitos de Ambiente
+                </h2><br>
+
+                Antes de iniciar a instalação, certifique-se de possuir:<br><br>
+
+                Um servidor em nuvem ou uma hospedagem tradicional.<br><br>
+
+                Exemplos
+                <ul style="list-style-type: '✔️'; padding-left: 1.5em;">
+                <li>Hospedagem tradicional: Hostinger, HostGator</li>
+                <li>Servidor em nuvem: AWS, Laravel Cloud, DigitalOcean</li>
+                </ul><br>
+
+                Versão do PHP compatível<br><br>
+
+                <ul style="list-style-type: '✔️'; padding-left: 1.5em;">
+                <li>Verifique a versão do PHP exigida pelo sistema</li>
+                <li>Garanta que a hospedagem ou servidor esteja com essa versão ativa</li>
+                </ul><br>
+
+                Extensões PHP necessárias:<br><br>
+
+                <ul style="list-style-type: '✔️'; padding-left: 1.5em;">
+                <li>BCMath</li>
+                <li>Ctype</li>
+                <li>Fileinfo</li>
+                <li>JSON</li>
+                <li>Mbstring</li>
+                <li>OpenSSL</li>
+                <li>PDO</li>
+                <li>Tokenizer</li>
+                <li>XML</li>
+                </ul><br>
+
+                <h2 class="text-2xl font-semibold flex items-center gab-3">
+                <span class="iconify text-2xl" data-icon="solar:round-alt-arrow-right-line-duotone"></span>
+                Configuração do Repositório
+                </h2><br><br>
+
+                Adicionar a Chave SSH no GitHub<br><br>
+
+                <ul style="list-style-type: '✔️'; padding-left: 1.5em;">
+                <li>Gere uma chave SSH localmente (caso ainda não possua)</li>
+                <li>Acesse seu repositório no Github</li>
+                <li>Vá em Configurações > SSH and GPG keys > New SSH key</li>
+                <li>Cole a chave pública gerada</li>
+                <li>Salve a nova chave SSH</li>
+                </ul><br><br>
+
+                Confirmar Implantação Automátiva<br><br>
+
+                <ul style="list-style-type: '✔️'; padding-left: 1.5em;">
+                <li>Acesse as configurações de repositório no GitHub</li>
+                <li>Vá até seção Webhooks</li>
+                <li>Clique em Add webhook</li>
+                <li>Copie e cole a URL do Webhook fornecida pelo seu servidor ou serviços de integração contínua</li>
+                <li>Selecione o evento desejado para o acionamento da implantação (geralmente "push")</li>
+                <li>Salve as alterações</li>
+                </ul><br><br>
+            </p>
         </div>
     </section>
     <script src="src/js/main.js"></script>
