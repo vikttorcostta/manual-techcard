3140bb5374cf0c75186641073b23460fe2ece4c7
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-04-29 15:15:15 -0300

    ♻️ refactor: ajuste no layout

--- src/pages/novaPagina.php
+++ src/pages/novaPagina.php
@@ -1,7 +1,7 @@
 <section class="p-4 flex-1 ml-0 sm:ml-64 sm:p-8">
     <h2 class="text-2xl font-semibold">Nova pagina</h2>
 
-    <section class="mt-8 bg-white p-6 rounded shadow flex flex-col items-center gap-2 sm:flex-row">
+    <section class="mt-16 bg-white p-6 rounded shadow flex flex-col items-center gap-2 sm:flex-row">
         <div>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor autem recusandae doloribus ducimus sequi deserunt laborum suscipit nemo, dolorem nisi aperiam doloremque quis fuga voluptas quos accusantium tempore atque perferendis?</p>
         </div>
