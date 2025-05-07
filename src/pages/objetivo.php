21a64a0c4d1b864f4d0cd4fa49bfb6927c6c38c0
Gabriel Gomes <g.evangelista@ba.estudante.senai.br>
2025-05-07 19:44:37 -0300

    Atualização do objetivo do manual

--- src/pages/objetivo.php
+++ src/pages/objetivo.php
@@ -7,9 +7,20 @@
     <h2 class="text-2xl font-semibold"><?= tituloPaginaDinamico($pagina); ?></h2>
 
     <section class="mt-16 bg-white p-6 rounded shadow flex flex-col items-center gap-2 sm:flex-row">
-        <div>
-            <p>teste .20</p>
+        <div class="flex flex-col gap-5 ">
+        <h1 class="text-3xl font-semibold flex items-center gap-2">
+    <iconify-icon icon="solar:bill-check-bold" class="text-4xl text-blue-600"></iconify-icon>
+    Objetivo do Manual do Projeto vCardHub
+</h1>
+
+             <p>O Manual do Projeto TECHCARD tem como principal propósito orientar o usuário na compreensão, instalação e utilização eficaz do sistema. Desenvolvido como parte do curso Técnico em Desenvolvimento de Sistemas do SENAI/BA – Camaçari, este manual fornece uma base sólida para garantir uma experiência fluida e completa com a plataforma TECHCARD, desde os primeiros passos até o uso das funcionalidades mais avançadas.</p>
+
+             <p>O documento é voltado tanto para usuários iniciantes quanto para usuários avançados e gestores, oferecendo uma abordagem prática, organizada e acessível.</p>
         </div>
+        
     </section>
+    <script src="https://code.iconify.design/iconify-icon/1.0.8/iconify-icon.min.js"></script>
+    <script src=""></script>
+
     <script src="src/js/main.js"></script>
 </section>
