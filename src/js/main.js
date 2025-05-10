78ceb1e30640832ef47b7ac3321c51417f5af0a3
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-05-10 10:56:17 -0300

    ♻️ refactor: foi removidas as complexidades referente a roteamento e o PHP

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,29 @@
-function exibirMenuLateral(){
-    document.getElementById('sidebar').classList.toggle('hidden');
+function exibirMenuLateral() {
+  document.getElementById('sidebar').classList.toggle('hidden');
 }
 
 function exibirSubmenu() {
   document.getElementById('submenu').classList.toggle('hidden');
 }
 
+function highlightAtivo() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.sidebar-link');
+    const sections = document.querySelectorAll('section[id], div[id]');
+
+    function changeLinkState() {
+      let index = sections.length;
+
+      while (--index && window.scrollY + 100 < sections[index].offsetTop) { }
+
+      links.forEach((link) => link.classList.remove('active-sidebar-link'));
+      links[index].classList.add('active-sidebar-link');
+    }
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+  });
+}
+
+highlightAtivo();
 
